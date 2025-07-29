2e2474f91d1859b2dcc74a116c86bc93301b7dfb
Gabriel <gabrieltkdnobrega63@gmail.com>
2025-07-29 09:02:08 -0300

    feat: add image retrieval endpoint and update dependencies

---
+++ app.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+
+@app.get("/{image:path}")
+async def get_image(image: str) -> FileResponse:
+    image_path = Path("./Hotta/Content", image)
+    if not image_path.exists() or not image_path.is_file():
+        raise HTTPException(
+            status_code=404,
+            detail="Image not found, please check the path.",
+        )
+    return FileResponse(image_path)
